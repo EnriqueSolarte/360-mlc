933cd6c685a4819e3ebff0ebadd842b00da49dbc
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2022-12-09 08:54:17 +0800

    Fix setup.py and py modules for MLC

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='360_mlc',
     version='0.1',
-    packages=find_packages(include=['mlc']),
+    packages=find_packages(),
     author = "Enrique Solarte and Justin Wu (吳京軒)",
     author_email = "enrique.solarte.pardo@gmail.com",
     description = ("Multi-view layout consistency MLC - NeurIPS 2022"),
+    python_requires='>=3.7',
     license = "BSD",
 )
