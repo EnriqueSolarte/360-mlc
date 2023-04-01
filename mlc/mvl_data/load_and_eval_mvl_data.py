b188d000d18c8eb98ccaff35428372cf56a18557
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2023-04-01 09:38:25 +0800

    Fix missed files
    * Added global_directories.yaml
    * Added load_and_eval_mvl_data.py
    * Fixed cfg for create mlc labels + added <Requiered flag>

--- mlc/mvl_data/load_and_eval_mvl_data.py
+++ mlc/mvl_data/load_and_eval_mvl_data.py
@@ -2,8 +2,12 @@ import os
 from pathlib import Path
 from mlc.config.cfg import read_config, read_omega_cfg
 from mlc.utils.io_utils import create_mlc_label_dirs
+from mlc.utils.image_utils import draw_boundaries_phi_coords, plot_image
 from mlc.mlc import iterator_room_scenes, compute_and_store_mlc_labels
+from mvl_challenge.utils.vispy_utils import plot_list_ly
+from imageio import imwrite
 import argparse
+import numpy as np
 
 
 def get_passed_args():
@@ -39,5 +43,13 @@ if __name__ == "__main__":
     create_mlc_label_dirs(cfg)
     
     for list_ly in iterator_room_scenes(cfg):
-        compute_and_store_mlc_labels(list_ly, save_vis=True)
+        for ly in list_ly:
+            img = ly.img
+            draw_boundaries_phi_coords(
+                img, phi_coords=np.vstack([ly.phi_coord[0], ly.phi_coord[1]])
+            )
+            # imwrite("test.jpg", img)
+            plot_image(image=img, caption=ly.idx)
+        plot_list_ly(list_ly)
+
     
