1c7d4aae0d17605dc336713e1415a954ddb2c4e8
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2024-11-24 21:38:52 +0800

    Add tag to v1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='360_mlc',
-    version='1.0',
+    version='1.0.0',
     packages=find_packages(),
     author = "Enrique Solarte and Justin Wu (吳京軒)",
     author_email = "enrique.solarte.pardo@gmail.com",
